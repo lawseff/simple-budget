88bee336e51f8871050b79af99d761cf7e4449c5
Mikhail Loseu <lawseff@gmail.com>
2025-12-09 18:21:26 +0100

    [backend] Fix application.yaml not being applied

--- backend/src/main/kotlin/io/github/lawseff/budget/Application.kt
+++ backend/src/main/kotlin/io/github/lawseff/budget/Application.kt
@@ -1,17 +1,11 @@
 package io.github.lawseff.budget
 
 import io.ktor.server.application.Application
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.jetty.jakarta.Jetty
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 
-
-fun main() {
-    embeddedServer(Jetty, port = 8080, module = Application::module)
-        .start(wait = true)
-}
+fun main(args: Array<String>): Unit = io.ktor.server.jetty.jakarta.EngineMain.main(args)
 
 fun Application.module() {
     routing {
